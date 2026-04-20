1a9446c9125b1e4071a73793d482b387b3ff7c43
AMoment <1315545571@qq.com>
2026-04-20 12:55:35 +0800

    Finish impl_v1 and impl_v2, working(debugging) v3

--- 4-C/C_my_fft.h
+++ 4-C/C_my_fft.h
@@ -54,26 +54,26 @@ void my_fft_util_print_complex_matrix(my_complex *matrix, size_t N0, size_t N1);
  *   my_fft_2d        / my_fft_forward_2d  / my_fft_backward_2d
  * ------------------------------------------------------------------------- */
 #ifdef MY_FFT_USE_RECURSIVE
-#  define MY_FFT_VERSION_STR "v1 (recursive)"
+#define MY_FFT_VERSION_STR "v1 (recursive)"
 
-#  define my_fft_1d          my_fft_1d_v1
-#  define my_fft_forward_1d  my_fft_forward_1d_v1
-#  define my_fft_backward_1d my_fft_backward_1d_v1
+#define my_fft_1d my_fft_1d_v1
+#define my_fft_forward_1d my_fft_forward_1d_v1
+#define my_fft_backward_1d my_fft_backward_1d_v1
 
-#  define my_fft_2d          my_fft_2d_v1
-#  define my_fft_forward_2d  my_fft_forward_2d_v1
-#  define my_fft_backward_2d my_fft_backward_2d_v1
+#define my_fft_2d my_fft_2d_v1
+#define my_fft_forward_2d my_fft_forward_2d_v1
+#define my_fft_backward_2d my_fft_backward_2d_v1
 
 #else /* default: iterative */
-#  define MY_FFT_VERSION_STR "v2 (iterative)"
+#define MY_FFT_VERSION_STR "v2 (iterative)"
 
-#  define my_fft_1d          my_fft_1d_v2
-#  define my_fft_forward_1d  my_fft_forward_1d_v2
-#  define my_fft_backward_1d my_fft_backward_1d_v2
+#define my_fft_1d my_fft_1d_v2
+#define my_fft_forward_1d my_fft_forward_1d_v2
+#define my_fft_backward_1d my_fft_backward_1d_v2
 
-#  define my_fft_2d          my_fft_2d_v2
-#  define my_fft_forward_2d  my_fft_forward_2d_v2
-#  define my_fft_backward_2d my_fft_backward_2d_v2
+#define my_fft_2d my_fft_2d_v2
+#define my_fft_forward_2d my_fft_forward_2d_v2
+#define my_fft_backward_2d my_fft_backward_2d_v2
 
 #endif /* MY_FFT_USE_RECURSIVE */
 
